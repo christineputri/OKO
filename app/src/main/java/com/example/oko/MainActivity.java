b43135699badda3081e0a11bf41e6f33e23c1f66
christineputri <89205581+christineputri@users.noreply.github.com>
2024-02-07 23:54:59 +0700

    SubscriptionActivity

--- app/src/main/java/com/example/oko/MainActivity.java
+++ app/src/main/java/com/example/oko/MainActivity.java
@@ -1,118 +1,118 @@
-package com.example.oko;
-
-import android.content.Intent;
-import android.content.SharedPreferences;
-import android.os.Bundle;
-import android.util.Log;
-import android.view.Menu;
-import android.view.MenuItem;
-import android.widget.Button;
-import android.widget.SearchView;
-import android.widget.TextView;
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.recyclerview.widget.LinearLayoutManager;
-import androidx.recyclerview.widget.RecyclerView;
-import com.android.volley.DefaultRetryPolicy;
-import com.android.volley.RequestQueue;
-import com.android.volley.Response;
-import com.android.volley.VolleyError;
-import com.android.volley.toolbox.JsonArrayRequest;
-import com.android.volley.toolbox.Volley;
-import com.example.oko.model.User;
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.Task;
-import com.google.firebase.firestore.DocumentSnapshot;
-import com.google.firebase.firestore.FirebaseFirestore;
-import com.google.firebase.firestore.QueryDocumentSnapshot;
-import com.google.firebase.firestore.QuerySnapshot;
-
-import org.json.JSONArray;
-import org.json.JSONException;
-import org.json.JSONObject;
-
-import java.util.ArrayList;
-
-public class MainActivity extends AppCompatActivity {
-
-    FirebaseFirestore db = FirebaseFirestore.getInstance();
-    TextView username, loadingMsg;
-    Button loginBtn;
-    String userId;
-    User user;
-    //    ArrayList<Product>products = new ArrayList<>();
-    RecyclerView recyclerView;
-//    ProductAdapter productAdapter;
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
-        username = findViewById(R.id.greetingsText);
-//        loginBtn = findViewById(R.id.btn_login);
-//        loadingMsg = findViewById(R.id.loading_msg);
-
-        SharedPreferences sharedPreferences = getSharedPreferences("User", MODE_PRIVATE);
-        userId = sharedPreferences.getString("UserID", null);
-        Log.e("Main", userId);
-        getUserById(userId);
-        getAllProduct();
-
-        // search
-//        SearchView searchView = findViewById(R.id.search_bar);
-//        search(searchView);
-
-//        loginBtn.setOnClickListener(e->{
-//            Intent toLogin = new Intent(MainActivity.this, LoginActivity.class);
+//package com.example.oko;
+//
+//import android.content.Intent;
+//import android.content.SharedPreferences;
+//import android.os.Bundle;
+//import android.util.Log;
+//import android.view.Menu;
+//import android.view.MenuItem;
+//import android.widget.Button;
+//import android.widget.SearchView;
+//import android.widget.TextView;
+//import androidx.annotation.NonNull;
+//import androidx.appcompat.app.AppCompatActivity;
+//import androidx.recyclerview.widget.LinearLayoutManager;
+//import androidx.recyclerview.widget.RecyclerView;
+//import com.android.volley.DefaultRetryPolicy;
+//import com.android.volley.RequestQueue;
+//import com.android.volley.Response;
+//import com.android.volley.VolleyError;
+//import com.android.volley.toolbox.JsonArrayRequest;
+//import com.android.volley.toolbox.Volley;
+//import com.example.oko.model.User;
+//import com.google.android.gms.tasks.OnCompleteListener;
+//import com.google.android.gms.tasks.Task;
+//import com.google.firebase.firestore.DocumentSnapshot;
+//import com.google.firebase.firestore.FirebaseFirestore;
+//import com.google.firebase.firestore.QueryDocumentSnapshot;
+//import com.google.firebase.firestore.QuerySnapshot;
+//
+//import org.json.JSONArray;
+//import org.json.JSONException;
+//import org.json.JSONObject;
+//
+//import java.util.ArrayList;
+//
+//public class MainActivity extends AppCompatActivity {
+//
+//    FirebaseFirestore db = FirebaseFirestore.getInstance();
+//    TextView username, loadingMsg;
+//    Button loginBtn;
+//    String userId;
+//    User user;
+//    //    ArrayList<Product>products = new ArrayList<>();
+//    RecyclerView recyclerView;
+////    ProductAdapter productAdapter;
+//
+//    @Override
+//    protected void onCreate(Bundle savedInstanceState) {
+//        super.onCreate(savedInstanceState);
+//        setContentView(R.layout.activity_main);
+//
+//        username = findViewById(R.id.greetingsText);
+////        loginBtn = findViewById(R.id.btn_login);
+////        loadingMsg = findViewById(R.id.loading_msg);
+//
+//        SharedPreferences sharedPreferences = getSharedPreferences("User", MODE_PRIVATE);
+//        userId = sharedPreferences.getString("UserID", null);
+//        Log.e("Main", userId);
+//        getUserById(userId);
+//        getAllProduct();
+//
+//        // search
+////        SearchView searchView = findViewById(R.id.search_bar);
+////        search(searchView);
+//
+////        loginBtn.setOnClickListener(e->{
+////            Intent toLogin = new Intent(MainActivity.this, LoginActivity.class);
+////            startActivity(toLogin);
+////        });
+//
+//    }
+//
+//    @Override
+//    public boolean onCreateOptionsMenu(Menu menu) {
+//        getMenuInflater().inflate(R.menu.menu, menu);
+//        return super.onCreateOptionsMenu(menu);
+//    }
+//
+//    @Override
+//    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+//
+//        int menuItem = item.getItemId();
+//
+//        if (menuItem == R.id.action_profile) {
+//            Intent toLogin = new Intent(this, ProfileActivity.class);
 //            startActivity(toLogin);
-//        });
-
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.menu, menu);
-        return super.onCreateOptionsMenu(menu);
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
-
-        int menuItem = item.getItemId();
-
-        if (menuItem == R.id.action_profile) {
-            Intent toLogin = new Intent(this, ProfileActivity.class);
-            startActivity(toLogin);
-        } else if (menuItem == R.id.action_sign_out) {
-            Intent toLogin = new Intent(this, LoginActivity.class);
-            startActivity(toLogin);
-            finish();
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
-
-    public void getUserById(String id) {
-        ArrayList<User> users = new ArrayList<>();
-        db.collection("users").document(id)
-                .get()
-                .addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
-                    @Override
-                    public void onComplete(@NonNull Task<DocumentSnapshot> task) {
-                        if (task.isSuccessful()) {
-                            DocumentSnapshot document = task.getResult();
-                            if (document.exists() && !document.getData().isEmpty()) {
-                                user = document.toObject(User.class);
-                                user.setId(document.getId());
-                                username.setText("Hello, " + user.getFirstName() + " " + user.getLastName() + "!");
-                            } else {
-                                Log.e("OKO", "No such document");
-                            }
-                        } else {
-                            Log.e("OKO", "get failed with ", task.getException());
-                        }
-                    }
-                });
-    }
-}
+//        } else if (menuItem == R.id.action_sign_out) {
+//            Intent toLogin = new Intent(this, LoginActivity.class);
+//            startActivity(toLogin);
+//            finish();
+//        }
+//
+//        return super.onOptionsItemSelected(item);
+//    }
+//
+//    public void getUserById(String id) {
+//        ArrayList<User> users = new ArrayList<>();
+//        db.collection("users").document(id)
+//                .get()
+//                .addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+//                    @Override
+//                    public void onComplete(@NonNull Task<DocumentSnapshot> task) {
+//                        if (task.isSuccessful()) {
+//                            DocumentSnapshot document = task.getResult();
+//                            if (document.exists() && !document.getData().isEmpty()) {
+//                                user = document.toObject(User.class);
+//                                user.setId(document.getId());
+//                                username.setText("Hello, " + user.getFirstName() + " " + user.getLastName() + "!");
+//                            } else {
+//                                Log.e("OKO", "No such document");
+//                            }
+//                        } else {
+//                            Log.e("OKO", "get failed with ", task.getException());
+//                        }
+//                    }
+//                });
+//    }
+//}
--- app/src/main/java/com/example/oko/ProfileActivity.java
+++ app/src/main/java/com/example/oko/ProfileActivity.java
@@ -1,135 +1,135 @@
-package com.example.oko;
-
-import android.content.Intent;
-import android.content.SharedPreferences;
-import android.os.Bundle;
-import android.util.Log;
-import android.view.MenuItem;
-import android.widget.Button;
-import android.widget.TextView;
-
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.ActionBar;
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.recyclerview.widget.LinearLayoutManager;
-import androidx.recyclerview.widget.RecyclerView;
-
-import com.example.skinmates.adapter.ReviewAdapter;
-import com.example.skinmates.model.Review;
-import com.example.skinmates.model.User;
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.Task;
-import com.google.firebase.firestore.DocumentSnapshot;
-import com.google.firebase.firestore.FirebaseFirestore;
-import com.google.firebase.firestore.QueryDocumentSnapshot;
-import com.google.firebase.firestore.QuerySnapshot;
-
-import java.util.ArrayList;
-
-public class ProfileActivity extends AppCompatActivity {
-    FirebaseFirestore db = FirebaseFirestore.getInstance();
-    TextView username, email, qty;
-    String userId;
-    User user;
-    ArrayList<Review>reviews;
-    ReviewAdapter reviewAdapter;
-    RecyclerView reviewRv;
-    Button signout;
-    TextView text;
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_profile);
-        username = findViewById(R.id.username);
-        email = findViewById(R.id.email);
-        qty = findViewById(R.id.qtyreview);
-        signout = findViewById(R.id.signoutbtn);
-        text = findViewById(R.id.text);
-        reviewRv = findViewById(R.id.review_rv_product);
-
-        ActionBar actionBar = getSupportActionBar();
-        actionBar.setDisplayHomeAsUpEnabled(true);
-        actionBar.setTitle("Profile");
-        actionBar.setHomeAsUpIndicator(R.drawable.back_arrow_icon);
-
-        SharedPreferences sharedPreferences = getSharedPreferences("User", MODE_PRIVATE);
-        userId = sharedPreferences.getString("UserID", null);
-        Log.e("Main", userId);
-        getUserById(userId);
-        getReviewsByUserID(userId);
-
-        signout.setOnClickListener(e->{
-            onOutClick();
-        });
-
-    }
-
-    public void onOutClick() {
-        Intent intent = new Intent(ProfileActivity.this, LoginActivity.class);
-        startActivity(intent);
-        finish();
-    }
-
-    public boolean onOptionsItemSelected(MenuItem item){
-        Intent toHome = new Intent(this, MainActivity.class);
-        startActivityForResult(toHome, 0);
-        finish();
-        return true;
-    }
-
-    public void getUserById(String id){
-        db.collection("users").document(id)
-                .get()
-                .addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
-                    @Override
-                    public void onComplete(@NonNull Task<DocumentSnapshot> task) {
-                        if (task.isSuccessful()) {
-                            DocumentSnapshot document = task.getResult();
-                            if (document.exists() && !document.getData().isEmpty()) {
-                                user = document.toObject(User.class);
-                                user.setId(document.getId());
-                                username.setText(user.getFirstName() + " " + user.getLastName() + "!");
-                                email.setText(user.getEmail());
-
-                            } else {
-                                Log.d("Skinmates", "No such document");
-                            }
-                        } else {
-                            Log.d("Skinmates", "get failed with ", task.getException());
-                        }
-                    }
-                });
-    }
-
-    public void getReviewsByUserID(String userId){
-        reviews = new ArrayList<>();
-        db.collection("reviews")
-                .whereEqualTo("userId", userId)
-                .get()
-                .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
-                    @Override
-                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
-                        if (task.isSuccessful() && !task.getResult().isEmpty()) {
-                            for (QueryDocumentSnapshot document : task.getResult()) {
-                                Review review = document.toObject(Review.class);
-                                reviews.add(review);
-                            }
-                            qty.setText(String.valueOf(reviews.size()));
-                            setReviewsRv();
-                        } else {
-                            Log.d("Skinmates", "get failed with ", task.getException());
-                        }
-                    }
-                });
-    }
-
-    public void setReviewsRv(){
-        reviewAdapter = new ReviewAdapter(this, reviews, "Product Reviews");
-        reviewRv.setAdapter(reviewAdapter);
-        reviewRv.setLayoutManager(new LinearLayoutManager(this));
-        if(reviewAdapter.getItemCount()!=0){
-            text.setText("");
-        }
-    }
-}
+//package com.example.oko;
+//
+//import android.content.Intent;
+//import android.content.SharedPreferences;
+//import android.os.Bundle;
+//import android.util.Log;
+//import android.view.MenuItem;
+//import android.widget.Button;
+//import android.widget.TextView;
+//
+//import androidx.annotation.NonNull;
+//import androidx.appcompat.app.ActionBar;
+//import androidx.appcompat.app.AppCompatActivity;
+//import androidx.recyclerview.widget.LinearLayoutManager;
+//import androidx.recyclerview.widget.RecyclerView;
+//
+//import com.example.skinmates.adapter.ReviewAdapter;
+//import com.example.skinmates.model.Review;
+//import com.example.skinmates.model.User;
+//import com.google.android.gms.tasks.OnCompleteListener;
+//import com.google.android.gms.tasks.Task;
+//import com.google.firebase.firestore.DocumentSnapshot;
+//import com.google.firebase.firestore.FirebaseFirestore;
+//import com.google.firebase.firestore.QueryDocumentSnapshot;
+//import com.google.firebase.firestore.QuerySnapshot;
+//
+//import java.util.ArrayList;
+//
+//public class ProfileActivity extends AppCompatActivity {
+//    FirebaseFirestore db = FirebaseFirestore.getInstance();
+//    TextView username, email, qty;
+//    String userId;
+//    User user;
+//    ArrayList<Review>reviews;
+//    ReviewAdapter reviewAdapter;
+//    RecyclerView reviewRv;
+//    Button signout;
+//    TextView text;
+//
+//    @Override
+//    protected void onCreate(Bundle savedInstanceState) {
+//        super.onCreate(savedInstanceState);
+//        setContentView(R.layout.activity_profile);
+//        username = findViewById(R.id.username);
+//        email = findViewById(R.id.email);
+//        qty = findViewById(R.id.qtyreview);
+//        signout = findViewById(R.id.signoutbtn);
+//        text = findViewById(R.id.text);
+//        reviewRv = findViewById(R.id.review_rv_product);
+//
+//        ActionBar actionBar = getSupportActionBar();
+//        actionBar.setDisplayHomeAsUpEnabled(true);
+//        actionBar.setTitle("Profile");
+//        actionBar.setHomeAsUpIndicator(R.drawable.back_arrow_icon);
+//
+//        SharedPreferences sharedPreferences = getSharedPreferences("User", MODE_PRIVATE);
+//        userId = sharedPreferences.getString("UserID", null);
+//        Log.e("Main", userId);
+//        getUserById(userId);
+//        getReviewsByUserID(userId);
+//
+//        signout.setOnClickListener(e->{
+//            onOutClick();
+//        });
+//
+//    }
+//
+//    public void onOutClick() {
+//        Intent intent = new Intent(ProfileActivity.this, LoginActivity.class);
+//        startActivity(intent);
+//        finish();
+//    }
+//
+//    public boolean onOptionsItemSelected(MenuItem item){
+//        Intent toHome = new Intent(this, MainActivity.class);
+//        startActivityForResult(toHome, 0);
+//        finish();
+//        return true;
+//    }
+//
+//    public void getUserById(String id){
+//        db.collection("users").document(id)
+//                .get()
+//                .addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+//                    @Override
+//                    public void onComplete(@NonNull Task<DocumentSnapshot> task) {
+//                        if (task.isSuccessful()) {
+//                            DocumentSnapshot document = task.getResult();
+//                            if (document.exists() && !document.getData().isEmpty()) {
+//                                user = document.toObject(User.class);
+//                                user.setId(document.getId());
+//                                username.setText(user.getFirstName() + " " + user.getLastName() + "!");
+//                                email.setText(user.getEmail());
+//
+//                            } else {
+//                                Log.d("Skinmates", "No such document");
+//                            }
+//                        } else {
+//                            Log.d("Skinmates", "get failed with ", task.getException());
+//                        }
+//                    }
+//                });
+//    }
+//
+//    public void getReviewsByUserID(String userId){
+//        reviews = new ArrayList<>();
+//        db.collection("reviews")
+//                .whereEqualTo("userId", userId)
+//                .get()
+//                .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+//                    @Override
+//                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
+//                        if (task.isSuccessful() && !task.getResult().isEmpty()) {
+//                            for (QueryDocumentSnapshot document : task.getResult()) {
+//                                Review review = document.toObject(Review.class);
+//                                reviews.add(review);
+//                            }
+//                            qty.setText(String.valueOf(reviews.size()));
+//                            setReviewsRv();
+//                        } else {
+//                            Log.d("Skinmates", "get failed with ", task.getException());
+//                        }
+//                    }
+//                });
+//    }
+//
+//    public void setReviewsRv(){
+//        reviewAdapter = new ReviewAdapter(this, reviews, "Product Reviews");
+//        reviewRv.setAdapter(reviewAdapter);
+//        reviewRv.setLayoutManager(new LinearLayoutManager(this));
+//        if(reviewAdapter.getItemCount()!=0){
+//            text.setText("");
+//        }
+//    }
+//}
